e54a5580af8bdf95b3ead69862d73014d83a05f7
Douglas Modena <d.modena@live.com>
2016-05-03 23:39:09 -0300

    Correcao de classe principal.

--- src/main/scala/view/TesteCadastro.scala
+++ src/main/scala/view/TesteCadastro.scala
@@ -8,8 +8,8 @@ import model.Produto
   */
 object TesteCadastro {
   def main(args : Array[String]): Unit = {
-    var pe = new Pessoa("John", 25)
-    var pr = new Produto("Viola", 25, 2.70)
+    val pe = new Pessoa("John", 25)
+    val pr = new Produto("Viola", 25, 2.70)
 
     println(pe.imprimir)
     println(pr.imprimir)
